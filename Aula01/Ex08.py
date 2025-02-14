e037c309e08c55f6a573e9d375b612d134780fa4
foxczie <shiguemoriale@gmail.com>
2025-02-14 20:42:09 -0300

    Update Ex08.py

--- Aula01/Ex08.py
+++ Aula01/Ex08.py
@@ -2,9 +2,16 @@
 # Use loops para gerar a sequência e estruturas condicionais para verificar o limite.
 
 numero = int(input("Insira um número: "))
-cont = 1
-soma = 0
+cont = 0
+f2 = 1
+f1 = f2 - 1
+
+print(f1)
+print(f2)
+
 while cont <= numero:
-    soma += cont
+    print(f1 + f2)
+    temp = f2
+    f2 += f1
+    f1 = temp
     cont += 1
-print(soma) 
